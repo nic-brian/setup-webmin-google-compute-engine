7dd73e9ff32adfd956ef9e1083c51930db151ebe
nic-brian <86538642+nic-brian@users.noreply.github.com>
2023-05-31 19:40:44 -0700

    placed code directly in the README

--- automation-script.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-cd /root
-curl -o setup-webmin-google-compute-engine.sh https://raw.githubusercontent.com/nic-brian/setup-webmin-google-compute-engine/main/main-script.sh
-sh setup-webmin-google-compute-engine.sh
