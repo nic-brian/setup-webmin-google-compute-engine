bd64a524bf82e208153e0e26c7c8695d19f963e4
nic-brian <86538642+nic-brian@users.noreply.github.com>
2023-05-31 20:33:00 -0700

    added packages

--- main-script.sh
+++ main-script.sh
@@ -3,5 +3,7 @@
 date
 cd /root
 apt update
-apt upgrade -y
+apt install -y mariadb-server
+apt install -y apache2
+apt install -y php php-curl php-json php-mysql php-imagick php-mbstring php-xml php-zip php-memcached php-redis php-bcmath php-intl imagemagick ghostscript
 date
