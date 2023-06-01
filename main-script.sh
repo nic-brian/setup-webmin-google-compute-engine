6d807d4c5d16d230f2f52f90e19a655256d1c8df
nic-brian <86538642+nic-brian@users.noreply.github.com>
2023-05-31 20:37:03 -0700

    add webmin setup

--- main-script.sh
+++ main-script.sh
@@ -6,4 +6,6 @@ apt update
 apt install -y mariadb-server
 apt install -y apache2
 apt install -y php php-curl php-json php-mysql php-imagick php-mbstring php-xml php-zip php-memcached php-redis php-bcmath php-intl imagemagick ghostscript
+curl -o setup-webmin.sh https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh
+sh setup-webmin.sh
 date
