cd1ef51bd9081ba00fe9eee106e62f3f798737f4
nic-brian <86538642+nic-brian@users.noreply.github.com>
2023-06-05 23:45:06 -0700

    automate more of the set up

--- main-script.sh
+++ main-script.sh
@@ -3,7 +3,15 @@
 date
 cd /root
 apt update
+
+# set up a generic domain name for this instance
 vhost=`curl https://dgl-dns-wzwqo2bdfa-uw.a.run.app/`
+
+# get user's email address
+echo What is your email address?
+read email
+
+# install required packages
 curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 bash add-google-cloud-ops-agent-repo.sh --also-install
 apt install -y mariadb-server
@@ -19,6 +27,42 @@ apt install -y snapd
 snap install core
 snap install --classic certbot
 ln -s /snap/bin/certbot /usr/bin/certbot
+
+# install wordpress
+pushd /var/www
+wget https://wordpress.org/latest.zip
+unzip latest.zip
+chown -R www-data:www-data wordpress
+popd
+
+# create virtual host
+pushd /etc/apache2/sites-available
+cat <<EOF >${vhost}conf
+<VirtualHost *:80>
+    DocumentRoot "/var/www/wordpress"
+    ServerName ${vhost::-1}
+    <Directory "/var/www/wordpress">
+        Options None
+        Require all granted
+    </Directory>
+</VirtualHost>
+EOF
+
+a2ensite ${vhost}conf
+systemctl reload apache2
+popd
+
+# provision TLS certificate
+certbot --apache -n --no-eff-email --agree-tos -m $email -d ${vhost::-1}
+
+# configure Webmin with new TLS certificate
+# TODO
+
+# create wordpress database, user, and permissions
+# TODO
+
+
+# open firewall for Webmin port
 gcloud compute firewall-rules create default-allow-webmin --action allow --target-tags webmin-server --source-ranges 0.0.0.0/0 --rules tcp:10000
 date
 passwd
